6db6d9ffd6230f64991e8748cf59c19efe6055d6
Sabyasachi Sarkar <54432341+sabyasachi073@users.noreply.github.com>
2023-07-03 11:56:04 +0530

    Update 50_Subset-II.cpp

--- 50_Subset-II.cpp
+++ 50_Subset-II.cpp
@@ -1,4 +1,33 @@
 #include <bits/stdc++.h> 
+
+/*
+// Brute Force Approach
+void helper(int idx, vector<int> &ds, vector<int> &arr, int n, set<vector<int>> &ans) {
+    if(idx==n) {
+        ans.insert(ds);
+        return;
+    }
+
+    // Take the element
+    ds.push_back(arr[idx]);
+    helper(idx+1, ds, arr, n, ans);
+
+    // Don't take the element
+    ds.pop_back();
+    helper(idx+1, ds, arr, n, ans);
+}
+
+vector<vector<int>> uniqueSubsets(int n, vector<int> &arr)
+{
+    set<vector<int>> ans;
+    vector<int> ds;
+    sort(arr.begin(), arr.end());
+    helper(0, ds, arr, n, ans);
+    return vector<vector<int>>(ans.begin(), ans.end());
+}
+*/
+
+// Optimized Approach
 void helper(int idx, vector<int> &ds, vector<int> &arr, int n, vector<vector<int>> &ans) {
     ans.push_back(ds);
 
