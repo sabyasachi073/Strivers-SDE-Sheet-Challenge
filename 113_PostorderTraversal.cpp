da031d07fdbc538c89fb302deea3262fb5656b48
Sabyasachi Sarkar <54432341+sabyasachi073@users.noreply.github.com>
2023-07-13 10:36:48 +0530

    Create 113_PostorderTraversal.cpp

---
+++ 113_PostorderTraversal.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+void postOrder(TreeNode *root, vector<int> &ans) {
+    if(root==NULL)
+        return;
+    
+    postOrder(root->left, ans);
+    postOrder(root->right, ans);
+    ans.push_back(root->data);
+}
+
+vector<int> getPostOrderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    postOrder(root, ans);
+    return ans;
+}
